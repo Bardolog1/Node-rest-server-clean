bb5fb4d6342a71bb4827249f6afaae7872fb00ca
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-04-10 18:57:35 -0500

    User entity is added

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,21 @@
+
+
+export class UserEntity{
+    
+    constructor(
+        public id : number,
+        public name : string,
+        public email : string,
+        public password : string,
+        public role : string [],
+        public img? : string,
+        
+    ){}
+    
+    //Definir las propiedades de la entidad
+     
+        
+    
+    
+
+}
