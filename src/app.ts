84c915d6cfc51a7f5955b2438560308a394ec8a3
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-04-10 17:02:31 -0500

    Init node project

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+(()=>{
+    main();
+})()
+
+
+
+async function main(){
+
+//TODO: await bade de datos
+
+//TODO: inicio del server
+
+console.log('Server is running on port 3000');
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+export class Server{
+    public readonly app = express();
+    constructor(){}
+    
+    
+    async start(){
+    
+    
+    }
+    
+
+}
