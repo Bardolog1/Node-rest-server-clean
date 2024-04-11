e34457bc4659046568c50fc7d42d3095b99b1c13
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-04-10 19:21:14 -0500

    DTO is implemented & method static create is added

---
+++ src/config/validators.ts
@@ -0,0 +1,12 @@
+
+
+export class Validators{
+
+
+    // regular expression for email validation
+    static get email(){
+        return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    }
+
+
+}
--- src/domain/entities/dtos/auth/register-user.dto.ts
+++ src/domain/entities/dtos/auth/register-user.dto.ts
@@ -1,3 +1,4 @@
+import { Validators } from "../../../../config";
 
 
 
@@ -11,11 +12,19 @@ export class RegisterUserDto {
 
     ) {}
     
+    
+    // metodo para crear una instancia de la clase RegisterUserDto y validar los datos 
     static create(object: {[key: string]: any}): [string?, RegisterUserDto?] {
     
+        const {name, email, password} = object;
         
+        if(!name) return ["Name is required", undefined];
+        if(!email) return ["Email is required", undefined];
+        if(!Validators.email.test(email)) return ["Email is invalid", undefined];
+        if(!password) return ["Password is required", undefined];
+        if(password.length < 6) return ["Password must be at least 6 characters", undefined];
     
     
-        return [];
+        return [undefined, new RegisterUserDto(name, email.toLowerCase(), password)];
     }
 }
